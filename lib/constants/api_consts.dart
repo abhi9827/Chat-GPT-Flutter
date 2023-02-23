52ae51dc75df22e44c44fb352ad65c39b93712d9
abhi9827 <abhilamixane1@gmail.com>
2023-02-23 12:02:56 +0545

    first commit

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+import 'package:chatgpt_course/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+
+Color scaffoldBackgroundColor = const Color(0xFF343541);
+Color cardColor = const Color(0xFF444654);
